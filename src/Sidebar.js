7f84f13068c0d83a35c12343f234b0cbe1954fd1
Umar9292 <umarpromd@gmail.com>
2020-08-26 16:33:42 +0500

    Working on sidebar component

---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar = () => (
+  <div className="sidebar">
+    <div className="sidebar__header"></div>
+    <div className="sidebar__search"></div>
+    <div className="sidebar__chat"></div>
+  </div>
+);
+
+export default Sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBoaCnovwk_2CxlTqyPZRYEwq111CDYFCk',
+  authDomain: 'whats-app-clone-41089.firebaseapp.com',
+  databaseURL: 'https://whats-app-clone-41089.firebaseio.com',
+  projectId: 'whats-app-clone-41089',
+  storageBucket: 'whats-app-clone-41089.appspot.com',
+  messagingSenderId: '586437095283',
+  appId: '1:586437095283:web:036d9b4422730188499f06',
+  measurementId: 'G-907G68DPMT',
+};
